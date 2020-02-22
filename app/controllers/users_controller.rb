c88fc9430942d21cd0b47df6555824a03a90a78f
mana <mn.ctgl.ws@gmail.com>
2020-02-22 06:41:02 +0000

    errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in
+  
   before_action :correct_user, only: [:edit, :update, :show, :buys] 
   
   def show
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
